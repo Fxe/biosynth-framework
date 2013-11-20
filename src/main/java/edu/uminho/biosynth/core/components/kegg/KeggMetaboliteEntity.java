3259752c376e097be2a709dc04ed81303fb09dc6
fxe <filipeliu@hotmail.com>
2013-11-20 21:24:49 +0000

    Kegg Metabolite

--- src/main/java/edu/uminho/biosynth/core/components/kegg/KeggMetaboliteEntity.java
+++ src/main/java/edu/uminho/biosynth/core/components/kegg/KeggMetaboliteEntity.java
@@ -30,6 +30,16 @@ public class KeggMetaboliteEntity extends GenericMetabolite{
 	private List<KeggMetaboliteCrossReferenceEntity> crossReferences = new ArrayList<> ();
 	
 	@ElementCollection
+	@CollectionTable(name="KEGG_METABOLITE_REACTION", joinColumns=@JoinColumn(name="ID_METABOLITE"))
+	@Column(name="REACTION")
+	protected List<String> reactions = new ArrayList<> ();
+	
+	@ElementCollection
+	@CollectionTable(name="KEGG_METABOLITE_PATHWAY", joinColumns=@JoinColumn(name="ID_METABOLITE"))
+	@Column(name="PATHWAY")
+	protected List<String> pathways = new ArrayList<> ();
+	
+	@ElementCollection
 	@CollectionTable(name="KEGG_METABOLITE_ENZYME", joinColumns=@JoinColumn(name="ID_METABOLITE"))
 	@Column(name="ENZYME")
 	protected List<String> enzymes = new ArrayList<> ();
@@ -75,10 +85,43 @@ public class KeggMetaboliteEntity extends GenericMetabolite{
 		this.crossReferences.add(crossReference);
 		crossReference.setKeggMetaboliteEntity(this);
 	}
+	
 	public List<String> getEnzymes() {
 		return enzymes;
 	}
 	public void setEnzymes(List<String> enzymes) {
 		this.enzymes = enzymes;
+	}
+	
+	public List<String> getReactions() {
+		return reactions;
+	}
+	public void setReactions(List<String> reactions) {
+		this.reactions = reactions;
+	}
+	
+	public List<String> getPathways() {
+		return pathways;
+	}
+	public void setPathways(List<String> pathways) {
+		this.pathways = pathways;
+	}
+	
+	@Override
+	public String toString() {
+		final char sep = '\n';
+		StringBuilder sb = new StringBuilder();
+		sb.append("entry:").append(entry).append(sep);
+		sb.append("name:").append(name).append(sep);
+		sb.append("formula:").append(formula).append(sep);
+		sb.append("molw:").append(molWeight).append(sep);
+		sb.append("mass:").append(mass).append(sep);
+		sb.append("remark:").append(remark).append(sep);
+		sb.append("comment:").append(comment).append(sep);
+		sb.append("enzymes:").append(enzymes).append(sep);
+		sb.append("pathways:").append(pathways).append(sep);
+		sb.append("reactions:").append(reactions).append(sep);
+		sb.append("xrefs:").append(crossReferences);
+		return sb.toString();
 	}
 }
