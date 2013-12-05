3a8ca73b1b2c45c458029dbed29c01854455c8c8
fxe <filipeliu@hotmail.com>
2013-12-05 21:50:10 +0000

    chebi work

---
+++ src/test/java/edu/uminho/biosynth/core/components/chebi/TestChEbiMetaboliteEntity.java
@@ -0,0 +1,25 @@
+package edu.uminho.biosynth.core.components.chebi;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestChEbiMetaboliteEntity {
+
+	@Test
+	public void test4164() {
+		/*
+		 * CHEBI:12317, CHEBI:4164, CHEBI:42901 - M:CHEBI:15873
+		 * alpha-D-glucosamine 6-phosphate
+		 * STARS: 3
+		 * FORMULA: C6H14NO8P
+		 * CHARGE: 0
+		 * MASS: 259.15106 
+		 * InChl: InChI=1S/C6H14NO8P/c7-3-5(9)4(8)2(15-6(3)10)1-14-16(11,12)13/h2-6,8-10H,1,7H2,(H2,11,12,13)/t2-,3-,4-,5-,6+/m1/s1
+		 * InChl Key: XHMJOUIAFHJHBW-UKFBFLRUSA-N
+		 * SMILES: N[C@H]1[C@@H](O)O[C@H](COP(O)(O)=O)[C@@H](O)[C@@H]1O
+		 */
+		fail("Not yet implemented");
+	}
+
+}
