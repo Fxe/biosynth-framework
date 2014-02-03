cce196f240b59cc510a6c2817bcdb9564c0b7e04
fxe <filipeliu@hotmail.com>
2014-02-03 12:03:17 +0000

    id gen

--- src/main/java/edu/uminho/biosynth/core/components/AbstractEntityIdGenerator.java
+++ src/main/java/edu/uminho/biosynth/core/components/AbstractEntityIdGenerator.java
@@ -24,8 +24,6 @@ public class AbstractEntityIdGenerator extends IdentityGenerator {
 		} else {
 			id = ((AbstractGenericEntity)obj).getId();
 		}
-		
-		System.out.println(id);
 		return id;
 	}
 }
