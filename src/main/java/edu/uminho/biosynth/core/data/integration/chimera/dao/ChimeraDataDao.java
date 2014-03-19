0014167f293cf2620c22fe60349d4c941e911376
Filipe Liu <filipeliu@hotmail.com>
2014-03-19 19:08:35 +0000

    chimera

--- src/main/java/edu/uminho/biosynth/core/data/integration/chimera/dao/ChimeraDataDao.java
+++ src/main/java/edu/uminho/biosynth/core/data/integration/chimera/dao/ChimeraDataDao.java
@@ -5,7 +5,7 @@ import java.util.Map;
 
 public interface ChimeraDataDao {
 	public List<Long> getAllMetaboliteIds();
-	public Map<String, Object> getEntry(Long id);
+	public Map<String, Object> getEntryProperties(Long id);
 	public List<Long> getClusterByQuery(String query);
 	public Map<String, List<Object>> getCompositeNode(Long id);
 }
