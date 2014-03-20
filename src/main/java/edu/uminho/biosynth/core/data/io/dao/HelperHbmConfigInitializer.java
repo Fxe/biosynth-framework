d5461a755ee71ba2d461e9a354747c18cb821d92
fxe <filipeliu@hotmail.com>
2014-03-20 02:39:33 +0000

    helper using file

--- src/main/java/edu/uminho/biosynth/core/data/io/dao/HelperHbmConfigInitializer.java
+++ src/main/java/edu/uminho/biosynth/core/data/io/dao/HelperHbmConfigInitializer.java
@@ -1,5 +1,7 @@
 package edu.uminho.biosynth.core.data.io.dao;
 
+import java.io.File;
+
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.service.ServiceRegistry;
@@ -9,6 +11,18 @@ public class HelperHbmConfigInitializer {
 
 	
 	public static SessionFactory initializeHibernateSession(String cfg) {
+		Configuration config = new Configuration().configure(cfg);
+//		Configuration config = new Configuration().configure("hibernate_debug_mysql.cfg.xml");
+		System.out.println(config.getProperty("hibernate.dialect"));
+		
+		ServiceRegistry servReg = 
+				new ServiceRegistryBuilder().applySettings(config.getProperties()).buildServiceRegistry();
+		SessionFactory sessionFactory = config.buildSessionFactory(servReg);
+		
+		return sessionFactory;
+	}
+	
+	public static SessionFactory initializeHibernateSession(File cfg) {
 		Configuration config = new Configuration().configure(cfg);
 //		Configuration config = new Configuration().configure("hibernate_debug_mysql.cfg.xml");
 		System.out.println(config.getProperty("hibernate.dialect"));
