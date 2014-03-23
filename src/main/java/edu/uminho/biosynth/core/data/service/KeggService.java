bf145ca9cdf06c6ab759ac348413db868a29f5eb
fxe <filipeliu@hotmail.com>
2014-03-23 17:00:45 +0000

    minor change

--- src/main/java/edu/uminho/biosynth/core/data/service/KeggService.java
+++ src/main/java/edu/uminho/biosynth/core/data/service/KeggService.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Service;
 import edu.uminho.biosynth.core.components.biodb.kegg.KeggCompoundMetaboliteEntity;
 import edu.uminho.biosynth.core.data.io.dao.IGenericDao;
 
-@Service
+
 public class KeggService extends AbstractMetaboliteService<KeggCompoundMetaboliteEntity> {
 	
 	public KeggService(IGenericDao dao) {
