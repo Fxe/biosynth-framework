7505e868a0ba7938dbf0ea4d4aebf204084ca1b4
fxe <filipeliu@hotmail.com>
2014-10-08 21:36:58 +0100

    retrofit

---
+++ src/test/java/edu/uminho/biosynth/mapping/TestRetrofitKeggApi.java
@@ -0,0 +1,39 @@
+package edu.uminho.biosynth.mapping;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import edu.uminho.biosynth.core.data.io.dao.biodb.kegg.KeggApi;
+import retrofit.RestAdapter;
+
+public class TestRetrofitKeggApi {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		RestAdapter restAdapter = new RestAdapter.Builder().setEndpoint("http://rest.kegg.jp/").build();
+		KeggApi keggApi = restAdapter.create(KeggApi.class);
+		System.out.println(keggApi.getInfo("ligand"));
+	}
+
+}
