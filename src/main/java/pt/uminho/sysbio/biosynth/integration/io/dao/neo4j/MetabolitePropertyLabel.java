d06cf2eab25b2e084d41b92c23dcb2a55de0379f
Filipe Liu <filipeliu@hotmail.com>
2014-12-10 12:06:00 +0000

    mcqs + metabolite strategies

--- src/main/java/pt/uminho/sysbio/biosynth/integration/io/dao/neo4j/MetabolitePropertyLabel.java
+++ src/main/java/pt/uminho/sysbio/biosynth/integration/io/dao/neo4j/MetabolitePropertyLabel.java
@@ -3,5 +3,6 @@ package pt.uminho.sysbio.biosynth.integration.io.dao.neo4j;
 import org.neo4j.graphdb.Label;
 
 public enum MetabolitePropertyLabel implements Label {
-	MolecularFormula, Name, SMILES, InChI, Charge, CanSMILES, IsotopeFormula
+	MolecularFormula, Name, SMILES, Charge, CanSMILES, IsotopeFormula,
+	InChI, FIKHB
 }
--- src/test/java/edu/uminho/biosynth/core/data/integration/chimera/service/TestHbmChimeraService.java
+++ src/test/java/edu/uminho/biosynth/core/data/integration/chimera/service/TestHbmChimeraService.java
@@ -20,9 +20,9 @@ import org.neo4j.graphdb.GraphDatabaseService;
 import pt.uminho.sysbio.biosynth.integration.IntegratedCluster;
 import pt.uminho.sysbio.biosynth.integration.IntegrationSet;
 import pt.uminho.sysbio.biosynth.integration.io.dao.hbm.HbmIntegrationMetadataDaoImpl;
+import pt.uminho.sysbio.biosynth.integration.strategy.metabolite.CrossreferenceTraversalStrategyImpl;
 import pt.uminho.sysbio.biosynthframework.biodb.helper.HelperHbmConfigInitializer;
 import edu.uminho.biosynth.core.data.integration.chimera.dao.Neo4jChimeraDataDaoImpl;
-import edu.uminho.biosynth.core.data.integration.chimera.strategy.CrossreferenceTraversalStrategyImpl;
 import edu.uminho.biosynth.core.data.integration.generator.PrefixKeyGenerator;
 import edu.uminho.biosynth.core.data.integration.neo4j.HelperNeo4jConfigInitializer;
 
