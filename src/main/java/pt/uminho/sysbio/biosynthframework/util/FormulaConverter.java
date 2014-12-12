85a62870e8e1a2a1d962ebb7024bdca0a4c6f074
fxe <filipeliu@hotmail.com>
2014-12-12 11:34:11 +0000

    formula conv

--- src/main/java/pt/uminho/sysbio/biosynthframework/util/FormulaConverter.java
+++ src/main/java/pt/uminho/sysbio/biosynthframework/util/FormulaConverter.java
@@ -1,5 +1,9 @@
 package pt.uminho.sysbio.biosynthframework.util;
 
+import java.util.Map;
+
 public interface FormulaConverter {
+	
+	public Map<String, Integer> getAtomCountMap(String formula);
 	public String convertToIsotopeMolecularFormula(String formula, boolean setOne);
 }
