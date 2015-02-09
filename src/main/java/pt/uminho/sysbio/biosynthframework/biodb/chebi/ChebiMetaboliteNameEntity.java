16c5b423d346f31253f0b06bc27d228ce91c5f1e
Filipe Liu <filipeliu@hotmail.com>
2015-02-09 10:04:56 +0000

    added neo4j annotations -> ChebiMetaboliteNameEntity

--- src/main/java/pt/uminho/sysbio/biosynthframework/biodb/chebi/ChebiMetaboliteNameEntity.java
+++ src/main/java/pt/uminho/sysbio/biosynthframework/biodb/chebi/ChebiMetaboliteNameEntity.java
@@ -8,6 +8,8 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import pt.uminho.sysbio.biosynthframework.annotations.MetaProperty;
+
 @Entity
 @Table(name="chebi_metabolite_name")
 public class ChebiMetaboliteNameEntity {
@@ -21,18 +23,23 @@ public class ChebiMetaboliteNameEntity {
 	@JoinColumn(name="metabolite_id")
 	private ChebiMetaboliteEntity chebiMetaboliteEntity;
 	
+	@MetaProperty
 	@Column(name="name", nullable=false)
 	private String name;
 	
+	@MetaProperty
 	@Column(name="type", nullable=false)
 	private String type;
 	
+	@MetaProperty
 	@Column(name="source", nullable=false)
 	private String source;
 	
+	@MetaProperty
 	@Column(name="adapted", nullable=false)
 	private String adapted;
 	
+	@MetaProperty
 	@Column(name="language", nullable=false)
 	private String language;
 	
